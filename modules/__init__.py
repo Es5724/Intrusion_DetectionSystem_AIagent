66c18f068ad680a685687b11d64b5cdab57d2a31
Es5724 <dksd5623@naver.com>
2025-05-17 04:27:44 +0900

    수리카타 미적용 수정

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,11 @@
-# Intrusion_DetectionSystem 모듈 패키지
+"""
+IDS 시스템 모듈 패키지
+"""
+# 모듈 가져오기를 편리하게 하기 위한 초기화 파일
+# 이 파일이 존재하면 Python이 디렉토리를 패키지로 인식합니다.
+
+# 버전 정보
+__version__ = '1.0.0'
 
 import os
 import sys
@@ -29,9 +36,25 @@ from .reinforcement_learning import NetworkEnv, DQNAgent, train_rl_agent, plot_t
 from .ml_models import MLTrainingWindow, train_random_forest, add_rf_predictions
 from .utils import is_colab, is_admin, run_as_admin, clear_screen, wait_for_enter, syn_scan
 
+# 수리카타 관리자 모듈 임포트 시도
+try:
+    from .suricata_manager import SuricataManager
+    SURICATA_SUPPORT = True
+except ImportError:
+    print("수리카타 매니저 모듈을 임포트할 수 없습니다. 수리카타가 정상적으로 설치되었는지 확인하세요.")
+    SURICATA_SUPPORT = False
+
+# 방어 모듈 마지막으로 임포트 (다른 모듈에 의존성이 있음)
+from .defense_mechanism import create_defense_manager, register_to_packet_capture
+
 __all__ = [
     'PacketCapture', 'PacketCaptureCore', 'preprocess_packet_data',
     'NetworkEnv', 'DQNAgent', 'train_rl_agent', 'plot_training_results', 'save_model', 'load_model',
     'MLTrainingWindow', 'train_random_forest', 'add_rf_predictions',
-    'is_colab', 'is_admin', 'run_as_admin', 'clear_screen', 'wait_for_enter', 'syn_scan'
+    'is_colab', 'is_admin', 'run_as_admin', 'clear_screen', 'wait_for_enter', 'syn_scan',
+    'create_defense_manager', 'register_to_packet_capture'
 ] 
+
+# 수리카타 지원이 있는 경우에만 export
+if SURICATA_SUPPORT:
+    __all__.append('SuricataManager') 
